80ce53ac34d443ee7aefe8accac67911895af97f
AlejandroBocanegra <a.boca@outlook.com>
2020-07-24 03:18:36 +0900

    initial push

---
+++ src/test/java/com/galvanize/flightlogapiproject/FlightLogApiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvanize.flightlogapiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightLogApiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
